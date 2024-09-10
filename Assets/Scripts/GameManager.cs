69f15be3a85583b63b69baddb7675cc023c086f3
lucca100S <luccacatalan@gmail.com>
2024-09-10 15:22:49 -0300

    Player health regeneration
    The player starts healing after hitting a 5 hit combo or higher

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,15 +12,44 @@ public class GameManager : MonoBehaviour
     private int points;
     public TMP_Text pointsText;
 
+    [SerializeField] private int combo = 0;
+    public TMP_Text comboText;
+
     public void AddPoint()
     {
         points++;
         pointsText.SetText(points.ToString());
+
+        combo++;
+        UpdateComboText();
+        Heal(10);
     }
 
     public void Damage(int damage)
     {
         health -= damage;
         healthBar.value = health;
+
+        // Breaks combo
+        combo = 0;
+        comboText.enabled = false;
+    }
+
+    void Heal(int heal)
+    {
+        if (health < 100 && combo >= 5)
+        {
+            health += heal;
+            healthBar.value = health;
+        }
+    }
+
+    void UpdateComboText()
+    {
+        comboText.SetText(combo + "  Combo");
+        if (combo >= 5)
+        {
+            comboText.enabled = true; // Enables combo count if combo is greater than 5
+        }
     }
 }
